5dbcae19e8744023d05486985af22153e8de8748
Average-Python-Enjoyer <113552282+Average-Python-Enjoyer@users.noreply.github.com>
2023-05-01 21:04:03 +0300

    Update func.h

--- L1/func.h
+++ L1/func.h
@@ -14,5 +14,7 @@ int create_array(struct GraphicsCard array[], int* size);
 void print_array(struct GraphicsCard array[], int size);
 int compare(const void* a, const void* b);
 void sort_array(struct GraphicsCard array[], int size, int field);
+void swap(struct GraphicsCard* a, struct GraphicsCard* b);
+void sort_by_gpu_clock(struct GraphicsCard array[], int size);
 
 #endif //INC_5_1_FUNCTIONS_H
